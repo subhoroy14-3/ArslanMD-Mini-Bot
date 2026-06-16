88b6108cf6a4897576533ccc0540dd018dc3202a
subhoroy143 <147959639+subhoroy14-3@users.noreply.github.com>
2026-06-16 18:11:36 +0530

    Update video creator attribution in fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -40,7 +40,7 @@ cmd({
     const caption = `*👑 FB VIDEO 👑*
 *👑 TIME :❯ ${result.duration}*
 *👑 CREATER :❯ ${data.creator}*
-*👑 BY :❯ ARSLAN-MD 👑*`;
+*👑 BY :❯ ZENIT-XMD 👑*`;
 
     await conn.sendMessage(
       from,
